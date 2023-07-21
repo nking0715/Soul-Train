243be971f7db4296ffd5b2f93089aff7d424e75d
TalentedBlu123 <talentedblu@gmail.com>
2023-07-21 05:28:09 -0400

    changed: port to 8000

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
