3e3c6bfc153646f9661e2a70d7369a6b1f09b75d
TalentedBlu123 <talentedblu@gmail.com>
2023-08-06 20:36:36 -0400

    changed Profile to profile

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,4 +1,4 @@
-const Profile = require('../models/Profile');
+const Profile = require('../models/profile');
 const { validationResult } = require('express-validator');
 
 exports.getProfile = async (req, res) => {
