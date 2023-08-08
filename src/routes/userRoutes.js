e6e18b90c09544eaa487ba92c307a719da9c16d0
TalentedBlu123 <talentedblu@gmail.com>
2023-08-08 05:29:32 -0400

    login with google added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/userController');
 const { check } = require('express-validator');
+const { OAuth2Client } = require('google-auth-library');
+require('dotenv').config();
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 router.post('/register', [
     check('username').notEmpty().withMessage('Username is required.'),
@@ -12,6 +16,28 @@ router.post('/register', [
         .matches(/[0-9]/).withMessage('Password must contain at least one number.')
         .matches(/[!@#$%^&*]/).withMessage('Password must contain at least one special character.')
 ], register);
+
 router.post('/login', login);
+
+router.post('/verify-google-token', async (req, res) => {
+    const idToken = req.body.idToken;
+
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: idToken,
+            audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        const userId = payload['sub'];
+
+        // Use the `userId` (or other payload fields) to identify the user in your system.
+        // Maybe fetch their profile from MongoDB, or create a new profile if it's their first time logging in.
+
+        res.send({ status: 'success', user: payload });
+
+    } catch (error) {
+        res.status(400).send({ status: 'error', message: 'Token verification failed.' });
+    }
+});
 
 module.exports = router;
