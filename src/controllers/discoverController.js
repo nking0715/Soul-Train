2f39de25a708093ee018f32436484b98121a614f
TalentedBlu123 <talentedblu@gmail.com>
2023-10-12 01:57:09 -0400

    ]

--- src/controllers/discoverController.js
+++ src/controllers/discoverController.js
@@ -4,14 +4,19 @@ const User = require('../models/user')
 
 exports.discoverContents = async (req, res) => {
     const { page, per_page } = req.body;
+    const userId = req.user.id;
     if (isEmpty(page) || isEmpty(per_page)) {
         return res.status(400).json({ success: false, message: "Invalid Request!" });
     }
     const start = (page - 1) * per_page; // Calculate the skip value
 
     try {
+        // Get the users that the requesting user follows
+        const user = await User.findById(userId);
+        const followedUserIds = user.following;
+
         const result = await Asset.aggregate([
-            { $match: { blocked: false, type: "video" } },
+            { $match: { blocked: false, type: "video", userId: { $nin: followedUserIds }  } },
             { $sort: { uploadedTime: 1 } }, // Sort assets by uploadedTime in ascending order
             { $skip: start }, // Skip the specified number of documents
             { $limit: per_page }, // Limit the number of documents
