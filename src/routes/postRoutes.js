0d939a3a3366fc49dcc5096f469f5ff4e4bb5647
TalentedBlu123 <talentedblu@gmail.com>
2023-11-03 01:21:21 +0800

    Added getSavedPost

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { createPost, commentPost, editComment, deleteComment, getComment, savePost } = require('../controllers/postController');
+const { createPost, commentPost, editComment, deleteComment, getComment, savePost, getSavedPost } = require('../controllers/postController');
 
 router.post('/', createPost);
 router.post('/comment', commentPost);
@@ -9,5 +9,6 @@ router.put('/comment', editComment);
 router.delete('/comment/:commentId', deleteComment);
 router.post('/getComment', getComment);
 router.post('/savePost', savePost);
+router.post('/getSavedPost', getSavedPost);
 
 module.exports = router;
