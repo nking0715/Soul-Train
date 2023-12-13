89d274d4997b6a3950bd75120bec2c3f8de4bc7c
TalentedBlu123 <talentedblu@gmail.com>
2023-12-13 10:59:18 +0800

    Updated notification, followManage

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -2,23 +2,13 @@ const admin = require('firebase-admin');
 
 exports.sendPushNotification = async (tokens, data, notification) => {
     try {
-        if (Array.isArray(tokens)) {
-            const messages = tokens.map(token => ({
-                token,
-                data,
-                notification
-            }));
-            console.log(messages);
-            await admin.messaging().sendEach(messages);
-        } else {
-            const message = {
-                token,
-                data,
-                notification
-            };
-            console.log(message);
-            await admin.messaging().send(message);
-        }
+        const messages = tokens.map(token => ({
+            token,
+            data,
+            notification
+        }));
+        console.log(messages);
+        await admin.messaging().sendEach(messages);
         return true;
     } catch (error) {
         console.log('Error in pushNotifications: ', error);
