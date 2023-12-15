76e524e9dedc397acfc56aa51b23ffd7fe900fe0
TalentedBlu123 <talentedblu@gmail.com>
2023-12-15 16:02:42 +0800

    Fixed search route error

--- src/routes/v1-api-router.js
+++ src/routes/v1-api-router.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const { discoverBlockedContents } = require("../controllers/discoverController");
-const { searchDancers } = require('../controllers/userController');
+const { search } = require('../controllers/userController');
 
 // App Backend Router
 router.post("/discover/blocked-contents", discoverBlockedContents);
-router.post('/search', searchDancers);
+router.get('/search', search);
 
 module.exports = router;
