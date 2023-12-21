6079625dd8384f96153dc03a4cbaba43fa720290
TalentedBlu123 <talentedblu@gmail.com>
2023-12-21 16:29:20 +0800

    Created editPost

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { createPost, getPost, deletePost, commentPost, editComment, deleteComment, getComment, likeComment,savePost, getSavedPost, reportContent, discoverPosts, homeFeed, likePost } = require('../controllers/postController');
+const { createPost, getPost, editPost, deletePost, commentPost, editComment, deleteComment, getComment, likeComment,savePost, getSavedPost, reportContent, discoverPosts, homeFeed, likePost } = require('../controllers/postController');
 
 router.post('/', createPost);
 router.get('/', getPost);
-router.delete('/', deletePost)
+router.patch('/', editPost);
+router.delete('/', deletePost);
 router.post('/comment', commentPost);
 router.put('/comment', editComment);
 router.delete('/comment', deleteComment);
