3d916b7a0ab9a71dafd259d6d534ea5c0db2020f
TalentedBlu123 <talentedblu@gmail.com>
2024-01-19 12:38:35 +0100

    feat: add signed url logic

--- src/controllers/matchController.js
+++ src/controllers/matchController.js
@@ -1,15 +1,44 @@
 const Match = require('../models/match');
 const User = require('../models/user');
+const { getSignedUrl } = require('../helper/aws.helper');
+
 require('dotenv').config();
+
+async function updateSignedURL(match) {
+    let videoUrl = match.videoUrl.split('.com/')[1];
+    const signedVideoUrl = await getSignedUrl(videoUrl);
+    let newMatch = match;
+    newMatch.videoUrl = signedVideoUrl
+    // Your async operation here, e.g., fetching data or processing
+    return newMatch; // Return the result of the async operation
+}
+
+async function processMatches(matches) {
+    try {
+        // Create an array of promises
+        const promises = matches.map(match => updateSignedURL(match));
+        
+        // Await all promises
+        const results = await Promise.all(promises);
+        // 'results' is an array containing the outcome of each async operation
+        return results;
+    } catch (error) {
+        // console.error('An error occurred:', error);
+        throw error;
+    }
+}
+
 
 exports.getMatchListByUserId = async (req, res) => {
     try {
         let { userId } = req.query;
-        const matchs = await Match.find({ users: { $in: [userId] } }).populate('users', 'username artistName'); // Only include username and artistName
-
+        let match = await Match.find({ users: { $in: [userId] } }).populate('users', 'username artistName'); // Only include username and artistName        
+        let videoUrl = match[0].videoUrl.split('.com/')[1];
+        videoUrl = await getSignedUrl(videoUrl);
+        match[0].videoUrl = videoUrl;
         return res.status(200).json({
             success: true,
-            matchs,
+            match,
         });
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message });
@@ -18,8 +47,7 @@ exports.getMatchListByUserId = async (req, res) => {
 
 exports.getMatchListWithFriends = async (req, res) => {
     try {
-        let { userId , perPage = 5, page = 1 } = req.query;
-
+        let { userId, perPage = 5, page = 1 } = req.query;
         // Calculate the number of results to skip (for pagination)
         const skip = (page - 1) * perPage;
         // Calculate the date 48 hours ago from now
@@ -28,7 +56,7 @@ exports.getMatchListWithFriends = async (req, res) => {
         // Retrieve the user and their friends list
         const user = await User.findById(userId).populate('follower');
         if (!user) {
-            console.log('User not found');
+            // console.log('User not found');
             return;
         }
 
@@ -36,26 +64,26 @@ exports.getMatchListWithFriends = async (req, res) => {
         const friendIds = user.follower.map(friend => friend._id);
 
         // Find matches where any of the friends are involved
-        const matches = await Match.find({ 
+        let matches = await Match.find({
             users: { $in: friendIds },
             createdAt: { $gte: date48HoursAgo } // Filter to include only matches created within the last 48 hours
         }).sort({ createdAt: -1 }) // Sorting by createdAt in descending order (newest first)
-        .skip(skip)              // Skip the previous pages' results
-        .limit(perPage)          // Limit the number of results
-        .populate('users', 'username artistName'); // Only include username and artistName
+            .skip(skip)              // Skip the previous pages' results
+            .limit(perPage)          // Limit the number of results
+            .populate('users', 'username artistName'); // Only include username and artistName
 
+        matches = await processMatches(matches);        
         // Check if matches were found
         if (matches.length > 0) {
-            console.log('Matches found with friends:', matches);
+            // console.log('Matches found with friends:', matches);
         } else {
-            console.log('No matches found with friends');
+            // console.log('No matches found with friends');
         }
 
         return res.status(200).json({
             success: true,
             matches,
         });
-
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message });
     }
@@ -68,13 +96,15 @@ exports.getMatchListByDiscovery = async (req, res) => {
         // Calculate the date 48 hours ago from now
         const date48HoursAgo = new Date(new Date().getTime() - (48 * 60 * 60 * 1000));
         const skip = (page - 1) * perPage;
-        const matches = await Match.find({ 
+        const matches = await Match.find({
             users: { $in: [userId] },
             createdAt: { $gte: date48HoursAgo } // Filter to include only matches created within the last 48 hours
         }).sort({ createdAt: -1 }) // Sorting by createdAt in descending order (newest first)
-        .skip(skip)              // Skip the previous pages' results
-        .limit(perPage)          // Limit the number of results
-        .populate('users', 'username artistName'); // Only include username and artistName
+            .skip(skip)              // Skip the previous pages' results
+            .limit(perPage)          // Limit the number of results
+            .populate('users', 'username artistName'); // Only include username and artistName
+
+        matches = await processMatches(matches);        
         return res.status(200).json({
             success: true,
             matches,
