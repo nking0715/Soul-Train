1d3328b2e9690d7aaa89fa56b7af4c4bb2f53b37
TalentedBlu123 <talentedblu@gmail.com>
2024-01-19 14:21:24 +0100

    fix: update apis

--- src/controllers/matchController.js
+++ src/controllers/matchController.js
@@ -13,11 +13,17 @@ async function updateSignedURL(match) {
     return newMatch; // Return the result of the async operation
 }
 
+let isUserIdInArray = (userIds, checkId) => {
+    userIds.some(userId =>
+        userId.toString() === checkId.toString()
+    );
+}
+
 async function processMatches(matches) {
     try {
         // Create an array of promises
         const promises = matches.map(match => updateSignedURL(match));
-        
+
         // Await all promises
         const results = await Promise.all(promises);
         // 'results' is an array containing the outcome of each async operation
@@ -33,9 +39,11 @@ exports.getMatchListByUserId = async (req, res) => {
     try {
         let { userId } = req.query;
         let match = await Match.find({ users: { $in: [userId] } }).populate('users', 'username artistName'); // Only include username and artistName        
-        let videoUrl = match[0].videoUrl.split('.com/')[1];
-        videoUrl = await getSignedUrl(videoUrl);
-        match[0].videoUrl = videoUrl;
+        if (match.length) {
+            let videoUrl = match[0]?.videoUrl.split('.com/')[1];
+            videoUrl = await getSignedUrl(videoUrl);
+            match[0].videoUrl = videoUrl;
+        }
         return res.status(200).json({
             success: true,
             match,
@@ -62,7 +70,7 @@ exports.getMatchListWithFriends = async (req, res) => {
 
         // Extract friend IDs from the followers
         const friendIds = user.follower.map(friend => friend._id);
-
+        let data = [];
         // Find matches where any of the friends are involved
         let matches = await Match.find({
             users: { $in: friendIds },
@@ -72,7 +80,18 @@ exports.getMatchListWithFriends = async (req, res) => {
             .limit(perPage)          // Limit the number of results
             .populate('users', 'username artistName'); // Only include username and artistName
 
-        matches = await processMatches(matches);        
+        matches = await processMatches(matches);
+        matches?.map(match => {
+            let newMatch = match;
+            if (isUserIdInArray(friendIds, match.playerA)) {
+                newMatch.follower = match.playerA;
+            }
+            if (isUserIdInArray(friendIds, match.playerB)) {
+                newMatch.follower = match.playerB;
+            }
+            // newMatch.
+            data.push(newMatch);
+        });
         // Check if matches were found
         if (matches.length > 0) {
             // console.log('Matches found with friends:', matches);
@@ -97,14 +116,13 @@ exports.getMatchListByDiscovery = async (req, res) => {
         const date48HoursAgo = new Date(new Date().getTime() - (48 * 60 * 60 * 1000));
         const skip = (page - 1) * perPage;
         let matches = await Match.find({
-            users: { $in: [userId] },
             createdAt: { $gte: date48HoursAgo } // Filter to include only matches created within the last 48 hours
         }).sort({ createdAt: -1 }) // Sorting by createdAt in descending order (newest first)
             .skip(skip)              // Skip the previous pages' results
             .limit(perPage)          // Limit the number of results
             .populate('users', 'username artistName'); // Only include username and artistName
 
-        matches = await processMatches(matches);        
+        matches = await processMatches(matches);
         return res.status(200).json({
             success: true,
             matches,
