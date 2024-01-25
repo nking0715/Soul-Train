9e45b96d6b7145db37d4ddb00de8719db24c1db4
TalentedBlu123 <talentedblu@gmail.com>
2024-01-25 15:58:49 +0100

    fix: update the music list

--- src/helper/socket.helper.js
+++ src/helper/socket.helper.js
@@ -2,8 +2,6 @@ const musicList = [
     'https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_Avion_FOR_BATTLE_V05.mp3',
     'https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_Dj+Arabinho+BOOM+BAP-_FOR_BATTLE_V05.mp3',
     'https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_DJ+Mordecai+-+Nautilus+Chops-_FOR_BATTLE_V05.mp3',
-    `https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_DJ+FLEG+-+06+We+Don't+Work+_FOR_BATTLE_V05.mp3`,
-    `https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_DJ+Mordecai+-+Nautilus+Chops-_FOR_BATTLE_V05.mp3`,
     `https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_Dj+Zapy+%26+Dj+Uragun+-+3+Rublya+Daj_FOR_BATTLE_V05.mp3`,
     `https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_djblesOne+-+04+Rich+Aliens-_FOR_BATTLE_V05.mp3`,
     `https://soul-train-bucket.s3.amazonaws.com/Music/FOR_BATTLE_V05_Falcons+Breaks+-+03+Drumline-_FOR_BATTLE_V05.mp3`,
